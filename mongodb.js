fb44f117ecd61afc239d34ea2d1397bd849cf448
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-15 14:42:17 +0100

    CRUD create with mongoDB

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,5 @@
+//learning CRUD in node
+
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 
@@ -11,8 +13,44 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    name:"Julien",
-    age: 27
+  // db.collection('users').insertOne({
+  //   name:"Julien",
+  //   age: 27
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user')
+  //   }
+  //   console.log(result.insertedId)
+  // })
+  // db.collection('users').insertMany([
+  //   {
+  //     name:"josh",
+  //     age: 20
+  //   }, {
+  //     name:"rio",
+  //     age:30
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert Document!')
+  //   }
+  //   console.log(result.insertedIds)
+  // })
+  db.collection('tasks').insertMany([
+    {
+      description:"study Node.js",
+      completed: false
+    }, {
+      description:'study React',
+      completed: false
+    }, {
+      description: 'study Ruby on Rails',
+      completed: true
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log('Unable to insert Document!')
+    }
+    console.log(result.insertedIds)
   })
 })
