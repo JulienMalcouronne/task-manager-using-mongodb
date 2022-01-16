bdbefc043db66a81a71bcb791a173bbdb7af03e3
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-16 11:39:27 +0100

    added validator package, some model validations and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,23 +1,64 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {})
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
   }
 })
 
-const me = new User({
-  name: 'Julien',
-  age: 28
+const Task = mongoose.model('Task', {
+  description: {
+    type: String
+  },
+  completed: {
+    type: Boolean
+  }
 })
 
-me.save().then(() => {
-  console.log(me)
-}).catch((error) => {
-  console.log('Error', error)
-})
+// const learn = new Task({
+//   description:'Learn Node.js',
+//   complete: false
+// })
+
+// learn.save().then(() => {
+//   console.log(learn)
+// }).catch((error) => {
+//   console.log('Error', error)
+// })
+
+// const me = new User({
+//   name: '   Julien   ',
+//   email: ' MYEMAIL@GMAIL.COM',
+//   age: 28
+// })
+
+// me.save().then(() => {
+//   console.log(me)
+// }).catch((error) => {
+//   console.log('Error', error)
+// })
