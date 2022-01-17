52efe6f0a1ff7ef16c00572fef44e7b2876bfb70
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-17 10:48:21 +0100

    added auth token for user signup and login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,23 @@ const userSchema = new mongoose.Schema({
           throw new Error('Age must be a positive number')
         }
       }
-    }
+    },
+    tokens: [{
+      token: {
+        type: String,
+        required: true
+      }
+    }]
 })
-
+// method accessible on instance of model
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+  return token
+}
+// static accessible on model
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
 
