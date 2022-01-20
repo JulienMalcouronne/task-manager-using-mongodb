479c86c1ce08a85d8d4f694a352cc5bb89bedc01
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-21 00:04:40 +0100

    added more test for upload and refacto tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -33,13 +33,25 @@ test('Should signup a new user', async () => {
     //assert that the database was changed correctly
     const user = await User.findById(response.body.user._id)
     expect(user).not.toBeNull()
+
+    // Assertions about the response
+    expect(response.body).toMatchObject({
+      user: {
+        name:'Andrew',
+        email:'andrew@example.com',
+      },
+      token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('MyPass777!')
 })
 
 test('Should login existing user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: userOne.email,
     password: userOne.password
   }).expect(200)
+  const user = await User.findById(userOneId)
+  expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login nonexistent user', async () => {
@@ -70,6 +82,8 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
@@ -77,4 +91,14 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+  await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+  const user = await User.findById(userOneId)
+  expect({}).toEqual({})
 })
