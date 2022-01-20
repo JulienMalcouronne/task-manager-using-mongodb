bcf867413c0d696ab4362af68e07fe0d0756f226
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-20 12:31:32 +0100

    addes some test on user

--- src/index.js
+++ src/index.js
@@ -1,49 +1,6 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
-
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled')
-//   } else {
-//     next()
-//   }
-// })
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is currently down. Check back soon')
-// })
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
-
-
-// const router = new express.Router()
-// router.get('/test', (req, res) => {
-//   res.send('This is from my other router')
-// })
-// app.use(router)
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async () => {
-//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
-
-//     const user = await User.findById('61e55d7847de5d785f95821f')
-//     await user.populate('tasks')
-//     console.log(user.tasks)
-// }
-
-// main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 const router = new express.Router()
-const sharp = require('sharp')
-const { sendWelcomeEmail } = require('../emails/account')
-const { sendCancelationEmail } = require('../emails/account')
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -99,31 +98,33 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
-  req.user.avatar = buffer
-  await req.user.save()
-  res.send()
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
 }, (error, req, res, next) => {
-  res.status(400).send({ error: error.message })
+    res.status(400).send({ error: error.message })
 })
 
 router.delete('/users/me/avatar', auth, async (req, res) => {
-  req.user.avatar = req.file.buffer
-  await req.user.save()
-  res.send()
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
 })
 
 router.get('/users/:id/avatar', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id)
-    if (!user || !user.avatar) {
-      throw new Error()
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
     }
-    res.set('Content-Type', 'image/png')
-    res.send(user.avatar)
-  } catch (e) {
-    res.status(404).send()
-  }
 })
 
 module.exports = router
