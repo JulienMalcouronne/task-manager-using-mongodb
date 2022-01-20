b4d18afac039454d278427cf6ea6e924ef1d1a24
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-21 00:11:00 +0100

    done for all user tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -100,5 +100,28 @@ test('Should upload avatar image', async () => {
         .attach('avatar', 'tests/fixtures/profile-pic.jpg')
         .expect(200)
   const user = await User.findById(userOneId)
-  expect({}).toEqual({})
+  // expect({}).toEqual({})
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user', async () => {
+  await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+          name:'Jess'
+        })
+        .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.name).toEqual('Jess')
+})
+
+test('Should not invalid user fields', async () => {
+  await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+          location: 'Philadelphia'
+        })
+        .expect(400)
 })
